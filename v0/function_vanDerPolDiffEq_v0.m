2f3d972a7c088b1e3460e24fe8bb39df8b953556
damian-dellavale <51412012+damian-dellavale@users.noreply.github.com>
2019-07-10 12:56:53 -0300

    Add files via upload

---
+++ v0/function_vanDerPolDiffEq_v0.m
@@ -0,0 +1,32 @@
+%Medical Physics Department at Bariloche Atomic Center, Argentina.
+%Author: Osvaldo Velarde (osva.m.velarde@gmail.com).
+%Project: Differential equation for the Van der Pol oscillator.
+%Date: 10/06/2019.
+
+% Van der Pol Equation.
+% d^2 (x) - mu*(1-x^2)* d(x) + w^2 * x = 0
+%
+% The VdP equation is equivalent to the system
+% dz/dt = F(z,mu,w)
+% where z = (x,y) and F = (y, mu*(1-x^2)*y - w^2 * x)
+
+% Inputs:
+% t ->  Time (scalar).
+% z ->  Variable (array 2 x 1).
+% mu -> Nonlinearity parameter of the VdP equation (scalar).
+% w ->  Angular frequency parameter of the VdP equation (scalar).
+
+% Outputs:
+% dzdt -> Derivative of vector "z" over time "t" (array 2 x 1).
+
+%Reference:
+%Velarde O, Urdapilleta E, Mato G, and Dellavale D (2019), Bifurcation
+%structure determines different phase-amplitude coupling patterns in the
+%activity of biologically plausible neural networks, NeuroImage, In Press,
+%(DOI: ...)
+
+function dzdt = function_vanDerPolDiffEq_v0(t,z,mu,w)
+    dzdt = [z(2);...
+            mu * (1 - z(1)^2) * z(2) - w^2 * z(1)];
+end
+
